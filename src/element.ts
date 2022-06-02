9878da5a97e706dda4a4f5d76f1bfaa29e67a4e5
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2022-06-02 15:47:21 +0300

    Move element functions to element.ts

--- src/element.ts
+++ src/element.ts
@@ -227,3 +227,208 @@ export declare class AlgebraElement extends ElementBaseType {
   static get dimensions(): number;
   static get size(): number;
 }
+
+// Comparisons using two arguments
+export function equals(a: AlgebraElement, b: AlgebraElement): boolean {
+  return a.equals(b);
+}
+export function closeTo(
+  a: AlgebraElement,
+  b: AlgebraElement,
+  tolerance?: number
+): boolean {
+  return a.closeTo(b, tolerance);
+}
+
+// Validation
+export function hasNaN(element: AlgebraElement): boolean {
+  return element.hasNaN();
+}
+export function hasInfinity(element: AlgebraElement): boolean {
+  return element.hasInfinity();
+}
+export function isNil(element: AlgebraElement, tolerance?: number): boolean {
+  return element.isNil(tolerance);
+}
+export function isGrade(
+  element: AlgebraElement,
+  grade: number,
+  tolerance?: number
+): boolean {
+  return element.isGrade(grade, tolerance);
+}
+
+// Unary scalar operations using one argument
+export function norm(element: AlgebraElement): number {
+  return element.norm();
+}
+export function vnorm(element: AlgebraElement): number {
+  return element.vnorm();
+}
+
+// Unary operations using one argument
+export function neg(element: AlgebraElement): AlgebraElement {
+  return element.neg();
+}
+export function cwAbs(element: AlgebraElement): AlgebraElement {
+  return element.cwAbs();
+}
+export function involute(element: AlgebraElement): AlgebraElement {
+  return element.involute();
+}
+export function rev(element: AlgebraElement): AlgebraElement {
+  return element.rev();
+}
+export function conjugate(element: AlgebraElement): AlgebraElement {
+  return element.conjugate();
+}
+export function inverse(element: AlgebraElement): AlgebraElement {
+  return element.inverse();
+}
+export function square(element: AlgebraElement): AlgebraElement {
+  return element.square();
+}
+export function normalize(
+  element: AlgebraElement,
+  newNorm?: number
+): AlgebraElement {
+  return element.normalize(newNorm);
+}
+export function rotorNormalize(element: AlgebraElement): AlgebraElement {
+  return element.rotorNormalize();
+}
+export function sqrt(element: AlgebraElement): AlgebraElement {
+  return element.sqrt();
+}
+export function rotorSqrt(element: AlgebraElement): AlgebraElement {
+  return element.rotorSqrt();
+}
+export function exp(
+  element: AlgebraElement,
+  forceTaylor?: boolean,
+  numTaylorTerms?: number
+): AlgebraElement {
+  return element.exp(forceTaylor, numTaylorTerms);
+}
+export function bivectorExp(element: AlgebraElement): AlgebraElement {
+  return element.bivectorExp();
+}
+export function log(element: AlgebraElement): AlgebraElement {
+  return element.log();
+}
+export function rotorLog(element: AlgebraElement): AlgebraElement {
+  return element.rotorLog();
+}
+export function clone(element: AlgebraElement) {
+  return element.clone();
+}
+// Dual Zoo
+export function dual(element: AlgebraElement): AlgebraElement {
+  return element.dual();
+}
+export function undual(element: AlgebraElement): AlgebraElement {
+  return element.undual();
+}
+export function podge(element: AlgebraElement): AlgebraElement {
+  return element.podge();
+}
+export function unpodge(element: AlgebraElement): AlgebraElement {
+  return element.unpodge();
+}
+// See star overload for forward implementation
+export function unstar(element: AlgebraElement): AlgebraElement {
+  return element.unstar();
+}
+export function hodge(element: AlgebraElement): AlgebraElement {
+  return element.hodge();
+}
+export function unhodge(element: AlgebraElement): AlgebraElement {
+  return element.unhodge();
+}
+
+// Scalar operations
+export function scale(element: AlgebraElement, scalar: number): AlgebraElement {
+  return element.scale(scalar);
+}
+export function pow(
+  element: AlgebraElement,
+  power: number,
+  splitStages?: number
+): AlgebraElement {
+  return element.pow(power, splitStages);
+}
+
+// Multi-scalar operations
+export function applyWeights(
+  element: AlgebraElement,
+  weights: number[]
+): AlgebraElement {
+  return element.applyWeights(weights);
+}
+
+// Index operations
+export function negateGrades(
+  element: AlgebraElement,
+  ...grades: number[]
+): AlgebraElement {
+  return element.negateGrades(...grades);
+}
+
+// Binary operations using two arguments
+export function add(a: AlgebraElement, b: AlgebraElement): AlgebraElement {
+  return a.add(b);
+}
+export function sub(a: AlgebraElement, b: AlgebraElement): AlgebraElement {
+  return a.sub(b);
+}
+export function mul(a: AlgebraElement, b: AlgebraElement): AlgebraElement {
+  return a.mul(b);
+}
+export function div(a: AlgebraElement, b: AlgebraElement): AlgebraElement {
+  return a.div(b);
+}
+export function ldivs(a: AlgebraElement, b: AlgebraElement): AlgebraElement {
+  return a.ldivs(b);
+}
+export function wedge(a: AlgebraElement, b: AlgebraElement): AlgebraElement {
+  return a.wedge(b);
+}
+export function vee(a: AlgebraElement, b: AlgebraElement): AlgebraElement {
+  return a.vee(b);
+}
+// Contractions
+export function contract(
+  a: AlgebraElement,
+  b: AlgebraElement,
+  criterion: (r: number, s: number) => number
+): AlgebraElement {
+  return a.contract(b, criterion);
+}
+export function dot(a: AlgebraElement, b: AlgebraElement): AlgebraElement {
+  return a.dot(b);
+}
+export function dotL(a: AlgebraElement, b: AlgebraElement): AlgebraElement {
+  return a.dotL(b);
+}
+export function dotR(a: AlgebraElement, b: AlgebraElement): AlgebraElement {
+  return a.dotR(b);
+}
+export function star(element: AlgebraElement): AlgebraElement;
+export function star(a: AlgebraElement, b: AlgebraElement): AlgebraElement;
+export function star(a: AlgebraElement, b?: AlgebraElement): AlgebraElement {
+  if (b === undefined) {
+    return a.star();
+  }
+  return a.star(b);
+}
+
+// Subsets
+export function imag(element: AlgebraElement): AlgebraElement {
+  return element.imag();
+}
+export function even(element: AlgebraElement): AlgebraElement {
+  return element.even();
+}
+export function grade(element: AlgebraElement, grade: number): AlgebraElement {
+  return element.grade(grade);
+}
